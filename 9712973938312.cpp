50d415bc7ad2f3a3173b750b20c0eb2412ad7eac
akiyamakai <grusha.cs@gmail.com>
2024-03-15 09:05:33 +0200

    Добавьте файлы проекта.

---
+++ 9712973938312.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main() {
+    const int SIZE = 5; 
+    int arr[SIZE] = { 5, 2, 8, 1, 9 };
+
+    int sum = 0;
+
+    
+    for (int i = 0; i < SIZE; ++i) {
+        sum += arr[i];
+    }
+
+   
+    double average = static_cast<double>(sum) / SIZE;
+
+   
+    std::cout << "Average value of array elements:: " << average << std::endl;
+
+    return 0;
+}
